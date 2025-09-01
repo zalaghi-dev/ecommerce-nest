8a76db219a08f1c0f19b646297ba091e24112bc5
AmirZalaghi <zalaghi.dev@gmail.com>
2025-09-01 12:44:27 +0330

    ADD: global middleware in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(new LoggerMiddleware().use.bind(new LoggerMiddleware()));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
