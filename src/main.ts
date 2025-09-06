52761470827151f637144ad72b51e92e8748d84b
AmirZalaghi <zalaghi.dev@gmail.com>
2025-09-06 22:43:48 +0330

    ADD: Swagger doc base setup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(new LoggerMiddleware().use.bind(new LoggerMiddleware()));
+
+  // -------------- Swagger Config --------------
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce nest API documention')
+    .setDescription(
+      'Just a simple App to understand features in nestjs, in the name of الله',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // -------------------------------------------
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
